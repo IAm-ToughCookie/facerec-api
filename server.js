ce3afd02a434c45a2ffa48eea065ec71fba90175
IAm-ToughCookie <toughcookie.dev@gmail.com>
2020-11-29 18:16:17 +0100

    trying to fix database connection

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(express.json());
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
