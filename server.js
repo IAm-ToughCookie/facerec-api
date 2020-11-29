5f7c3818b76ea22bb5648073f58ff66b631c6acb
IAm-ToughCookie <toughcookie.dev@gmail.com>
2020-11-29 18:03:20 +0100

    database changed

--- server.js
+++ server.js
@@ -16,10 +16,8 @@ app.use(express.json());
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: '',
-        password: '',
-        database: 'facerec-db'
+        host: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
